5199e1f2f7102a5157be3d6404743c4865eb678a
nlp-omoBp <2032900037@qq.com>
2026-01-16 18:36:11 +0800

    Update OpenAI_Qwen大语言模型&fastapi_文本分类.py

--- 韦潇/week01/作业2/OpenAI_Qwen大语言模型&fastapi_文本分类.py
+++ 韦潇/week01/作业2/OpenAI_Qwen大语言模型&fastapi_文本分类.py
@@ -10,7 +10,7 @@ app = FastAPI()
 client = OpenAI(
     # 若没有配置环境变量，请用百炼API Key将下行替换为：api_key="sk-xxx",
     # 申请地址：https://bailian.console.aliyun.com/?tab=model#/api-key
-    api_key="sk-557a9a5dd00044c2bd7262c662a11342", # 账号绑定，用来计费的
+    api_key="sk-557a9a5ddxxxxx2a11342", # 账号绑定，用来计费的
 
     # 大模型厂商的地址，阿里云
     base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
